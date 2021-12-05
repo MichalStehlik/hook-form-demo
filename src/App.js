7cd3f3a9ac2d818681517fe9a3277ee63759340f
Michal Stehlík <michal.stehlik@pslib.cz>
2021-12-05 22:34:48 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { useForm, Controller } from "react-hook-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// https://react-hook-form.com/
+
+const App = () => {
+  const { register, handleSubmit, control, formState: {errors} } = useForm();
+  const onSubmit = data => alert(JSON.stringify(data, " ", 4));
+  return(
+    <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Controller name="name" control={control} render={({ field }) => <Input {...field} />} />
+          {errors.name?.type === 'required' && <span>Name is required</span>}
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Controller name="password" control={control} render={({ field }) => <Input type="password" placeholder="password placeholder" {...field} />} />
+          {errors.password?.type === 'required' && <span>Password is required</span>}
+        </FormGroup>
+        <FormGroup check>
+          <Controller name="check" control={control} render={({ field }) => <Input type="checkbox" {...field} />} />
+         {' '}
+          <Label check>Check me out</Label>
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
   );
 }
 
